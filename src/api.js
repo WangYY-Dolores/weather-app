9081fc5ab9e7b29473232be7785a979fd137d87c
王媛原 <1299795278@qq.com>
2023-02-11 18:05:03 -0500

    initial commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bc8adbdeb6msh8680cac6b80956ep125621jsnc4cf7e88eb63',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "6c8b74b4828a3a613703714c93ec3284";
